89da0dc54d4bd42be11b556129a08671412696b5
KAVIN AKASH <kavinakash06@gmail.com>
2024-06-20 13:30:14 +0530

    first

---
+++ src/main/java/com/kavin/spring/todoapp/hello/HelloController.java
@@ -0,0 +1,15 @@
+package com.kavin.spring.todoapp.hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller  //make this as a bean
+public class HelloController {
+
+    @RequestMapping("say-hello")  //localhost:8080/say-hello
+    @ResponseBody
+    public String sayHello(){
+        return "How are you? From Hello Controller";
+    }
+}
